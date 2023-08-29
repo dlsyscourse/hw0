fixing typos and normalizing issue

src/simple_ml.py
@@ -40,8 +40,8 @@ def parse_mnist(image_filename, label_filename):
                 dimension of the data, e.g., since MNIST images are 28x28, it 
                 will be 784.  Values should be of type np.float32, and the data 
                 should be normalized to have a minimum value of 0.0 and a 
-                maximum value of 1.0. The normalization should be applied uniformly
-                across the whole dataset, _not_ individual images.
+                maximum value of 1.0 (i.e., scale original values of 0 to 0.0 
+                and 255 to 1.0).
 
             y (numpy.ndarray[dtype=np.uint8]): 1D numpy array containing the
                 labels of the examples.  Values should be of type np.uint8 and
@@ -61,7 +61,7 @@ def softmax_loss(Z, y):
         Z (np.ndarray[np.float32]): 2D numpy array of shape
             (batch_size, num_classes), containing the logit predictions for
             each class.
-        y (np.ndarray[np.int8]): 1D numpy array of shape (batch_size, )
+        y (np.ndarray[np.uint8]): 1D numpy array of shape (batch_size, )
             containing the true label of each example.
 
     Returns: